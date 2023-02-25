358e8813f111f65c4d1ed8c1769585a879f8431f
nomshamas <nomshamas@gmail.com>
2023-02-25 23:58:34 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+
+function changePara() {
+	document.getElementById('p-hide').innerText = "I started with a dream to influence the future of world. I always wanted to modernize the world through my services. In 2020, I decided to provide services to our respective clients." + "\n\n" + "Secondly, I didn't wanted to do job in future because I want to work in freelance market and don't want to work 9 to 5; I also wanted to provide jobs not only in our country but around the world. I adapted the modern techniques and continuously growing day-by-day.";
+}
